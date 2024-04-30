3f4c6ba1216da143bef7d864f7ba0b332432997d
jxmot <webdev@stupiddog.net>
2024-04-29 23:15:47 -0500

    initial commit, tests and usage examples

---
+++ test.js
@@ -0,0 +1,38 @@
+/* ************************************************************************ */
+// Events
+const EventEmitter = require('events');
+const evts = new EventEmitter();
+
+let wxsvcUpReady = false;
+let wxsvcFcReady = false;
+
+const cfg = require('./cfg/wxsvc-cfg.js');
+const wsvc = require(cfg.api);
+wsvc.init(evts, console.log);
+
+evts.on('WSVC_UPDATE', (m) => {
+    wxsvcUpReady = true;
+    //var data = Object.assign({}, m, {tstamp : Date.now()});
+    console.log(`WSVC_UPDATE : ${JSON.stringify(m)}`);
+});
+
+evts.on('WSVC_FORCST', (m) => {
+    wxsvcFcReady = true;
+    //var data = Object.assign({}, m, {tstamp : Date.now()});
+    console.log(`WSVC_FORCST : ${JSON.stringify(m)}`);
+});
+
+evts.emit('WSVC_START');
+
+function retrieve() {
+    //var data = Object.assign({}, {co: wsvc.currobsv, fc: wsvc.forecast}, {tstamp : Date.now()});
+    console.log(`retrieve current  : ${JSON.stringify(wsvc.currobsv)}`);
+    console.log(`retrieve forecast : ${JSON.stringify(wsvc.forecast)}`);
+};
+
+let invId = setInterval(() => {
+    if(wxsvcUpReady && wxsvcFcReady) {
+        retrieve();
+        clearInterval(invId);
+    }
+}, 5000);
