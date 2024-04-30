4ed2b00661454918fe2e423b3ab98183bae70612
jxmot <webdev@stupiddog.net>
2024-04-29 23:17:36 -0500

    initial commit, openweathermap 2.5 api

---
+++ cfg/wxsvc-openwm-cfg.js
@@ -0,0 +1,73 @@
+/*
+    WX Service Configuration
+
+    https://openweathermap.org/api
+*/
+let key = require('./_openwm-key.js');
+
+module.exports = {
+    location: { 
+        loc: [41.9691,-87.7507],// neighborhood
+        zip: '60630',
+        city: 'Chicago',
+        code: '4887398',        // chicago
+        state: 'Illinois',
+        staid: 'KORD',          // O'Hare
+        country: {
+            fullname: 'United States of America', 
+            alphacode: ['US', 'USA'], 
+            numcode: 840
+        }
+    },
+
+/*
+    stations: [
+        {id: 'KORD',loc: [41.9797,-87.9044],label: 'Chicago-O\'Hare International Airport'},
+        {id: 'KMDW',loc: [41.7842,-87.7553],label: 'Chicago Midway Airport'},
+        {id: 'KPWK',loc: [42.1208,-87.9047],label: 'Wheeling, Pal-Waukee Airport'},
+        {id: 'KDPA',loc: [41.8964,-88.2511],label: 'West Chicago, Dupage Airport'},
+        {id: 'KLOT',loc: [41.6031,-88.1016],label: 'Lewis University Airport'},
+        {id: 'KUGN',loc: [42.4255,-87.8634],label: 'Waukegan Regional Airport'},
+        {id: 'KARR',loc: [41.7713,-88.4815],label: 'Aurora Municipal Airport'},
+        {id: 'KJOT',loc: [41.5176,-88.1790],label: 'Joliet Regional Airport'},
+        {id: 'KENW',loc: [42.5950,-87.9381],label: 'Kenosha, Kenosha Regional Airport'},
+        {id: 'KDKB',loc: [41.9338,-88.7066],label: 'De Kalb Taylor Municipal Airport'},
+        {id: 'KIKK',loc: [41.0687,-87.8537],label: 'Greater Kankakee Airport'}
+    ],
+
+    default_station: 0,
+*/
+    service: {
+        name: 'OpenWeatherMap',
+        appid: key.data,
+        hostname: 'api.openweathermap.org',
+        docurl: 'https://openweathermap.org/api',
+        iconurl: 'https://openweathermap.org/img/w/',
+        datamode: 'json'
+    },
+
+    config: {
+        // 1 hour = 3600000
+        updintvl: 3600000 * 1.5,
+        // add 5min to stagger API calls
+        forintvl: (3600000 * 1.5) + 300000
+    },
+
+    urlparts : [
+        '/data/2.5',
+        '/weather',
+        '/forecast',
+//'/find',
+        '?lat=',
+        '&lon=',
+//'&cnt=',
+        '?zip=',
+        '?id=',
+        '&units=imperial',
+        '&mode=',
+        '&appid='
+    ],
+
+    useragent: 'SensorNet Node App',
+};
+
---
+++ openwthr-api-v25.js
@@ -0,0 +1,320 @@
+/*
+    Interface to the Open Weather Map API
+
+
+        https://openweathermap.org/current
+
+        https://openweathermap.org/forecast5
+
+
+    All APIs : 
+        https://openweathermap.org/api
+
+    Icons:
+        https://openweathermap.org/weather-conditions
+*/
+module.exports = (function()  {
+
+    wxsvc = {
+        currobsv: {},
+        forecast: {}
+    };
+
+    const https = require('https');
+
+    let wcfg = require('./cfg/wxsvc-openwm-cfg.js');
+
+    const UPARTS_PATHBASE = 0;
+    const UPARTS_WEATHER  = UPARTS_PATHBASE + 1;
+    const UPARTS_FORECAST = UPARTS_WEATHER + 1;
+    const UPARTS_LAT      = UPARTS_FORECAST + 1;
+    const UPARTS_LON      = UPARTS_LAT + 1;
+    const UPARTS_ZIP      = UPARTS_LON + 1;
+    const UPARTS_ID       = UPARTS_ZIP + 1;
+    const UPARTS_UNITS    = UPARTS_ID + 1;
+    const UPARTS_MODE     = UPARTS_UNITS + 1;
+    const UPARTS_APPID    = UPARTS_MODE + 1;
+
+    const LOC_LAT         = 0;
+    const LOC_LON         = 1;
+
+    let sys_evts = {};
+
+    let log = undefined;
+
+    /*
+    */
+    wxsvc.init = function(evts, _log = undefined) {
+        // will need to send events that will trigger
+        // a data transfer to the clients
+        sys_evts = evts;
+
+        log = _log;
+
+        sys_evts.on('WSVC_START', () => {
+            getCurrent(wcfg.location);
+            if(wcfg.config.updintvl !== 0) {
+                setInterval(getCurrent, wcfg.config.updintvl, wcfg.location);
+            }
+
+            getForecast(wcfg.location);
+            if(wcfg.config.forintvl !== 0) {
+                setInterval(getForecast, wcfg.config.forintvl, wcfg.location);
+            }
+        });
+    };
+
+    /*
+    */
+    function getCurrent(location) {
+
+        let path = wcfg.urlparts[UPARTS_PATHBASE] + wcfg.urlparts[UPARTS_WEATHER];
+        path = path + wcfg.urlparts[UPARTS_ID] + location.code;
+        path = path + wcfg.urlparts[UPARTS_UNITS];
+        path = path + wcfg.urlparts[UPARTS_MODE] + wcfg.service.datamode;
+        path = path + wcfg.urlparts[UPARTS_APPID];
+        path = path + wcfg.service.appid;
+
+        let opt = {
+            hostname: wcfg.service.hostname,
+            method: 'GET',
+            path: path,
+            headers: {
+                'user-agent':wcfg.useragent
+            }
+        };
+
+        let req = https.request(opt, res => {
+            let data = '';
+            let origin = {sta: wcfg.location.zip, plc: wcfg.location.city+', '+wcfg.location.state};
+
+            res.on('data', (chunk) => {
+                data += chunk;
+            });
+
+            res.on('end', function(d) {
+                log('getCurrent status code: ' + res.statusCode);
+                if(res.statusCode === 200) {
+                    parseStationCurrent(data.toString(), origin);
+                } else log('getCurrent ERROR from '+wcfg.service.name);
+            });
+        });
+
+        req.on('error', (err) => {
+            log('getCurrent Error: ' + err);
+        }); 
+    
+        // send the request
+        req.end();
+    };
+
+    /*
+    */
+    function parseStationCurrent(data, origin) {
+        // console.log(data);
+
+        let upd = {};
+        let raw = JSON.parse(data);
+
+        upd.format = 'owm-v25';
+
+        upd.svc = wcfg.service.name;
+        // url for retrieving icons
+        upd.iconurl = wcfg.service.iconurl;
+
+        upd.sta = origin.sta;
+        upd.plc = origin.plc;
+        
+        // date/time of observation
+        // NOTE : openwm time in forecast is UTC!
+        let d = new Date(0);
+        d.setUTCSeconds(raw.dt);
+        upd.gmt = d.getTime();
+
+        // sunrise & sunset times
+        d = new Date(0);
+        d.setUTCSeconds(raw.sys.sunrise);
+        upd.sr  = d.getTime();
+        
+        d = new Date(0);
+        d.setUTCSeconds(raw.sys.sunset);
+        upd.ss  = d.getTime();
+
+        // date/time of when data was collected
+        upd.tstamp = Date.now();
+
+        upd.t    = raw.main.temp;
+        upd.h    = raw.main.humidity;
+        upd.wd   = raw.wind.deg;
+        upd.ws   = raw.wind.speed;
+        upd.tmax = raw.main.temp_max;
+        upd.tmin = raw.main.temp_min;
+
+        upd.desc = raw.weather[0].description;
+        upd.main = raw.weather[0].main;
+        upd.icon = wcfg.service.iconurl + raw.weather[0].icon +'.png';
+
+        // make a copy without references
+        wxsvc.currobsv = JSON.parse(JSON.stringify(upd));
+
+        sys_evts.emit('WSVC_UPDATE', wxsvc.currobsv);
+    };
+
+    /*
+    */
+    function getForecast(loc) {
+
+        let path = wcfg.urlparts[UPARTS_PATHBASE] + wcfg.urlparts[UPARTS_FORECAST];
+
+        path = path + wcfg.urlparts[UPARTS_ID] + wcfg.location.code;
+        path = path + wcfg.urlparts[UPARTS_UNITS];
+        path = path + wcfg.urlparts[UPARTS_MODE] + wcfg.service.datamode;
+        path = path + wcfg.urlparts[UPARTS_APPID];
+        path = path + wcfg.service.appid;
+
+        let opt = {
+            hostname: wcfg.service.hostname,
+            method: 'GET',
+            path: path,
+            headers: {
+                'user-agent':wcfg.useragent
+            }
+        };
+
+        let req = https.request(opt, res => {
+            let data = '';
+            let origin = {sta: wcfg.location.zip, plc: wcfg.location.city+', '+wcfg.location.state};
+
+            res.on('data', (chunk) => {
+                data += chunk;
+            });
+
+            res.on('end', function() {
+                log('getForecast status code: ' + res.statusCode);
+                if(res.statusCode === 200) {
+                    parseForecast(data.toString(), origin);
+                } else log('getForecast ERROR from OWM');
+            });
+        });
+
+        req.on('error', (err) => {
+            log('getForecast Error: ' + err);
+        }); 
+    
+        // send the request
+        req.end();
+    };
+
+    /*
+    */
+    function parseForecast(data, origin) {
+
+        let fcast = {};
+        let per = {};
+
+        let raw = JSON.parse(data);
+
+        fcast.format = 'owm-v25';
+
+        // the data provider
+        fcast.svc = wcfg.service.name;
+        // url for retrieving icons
+        fcast.iconurl = wcfg.service.iconurl;
+        // station code & named location
+        fcast.sta = origin.sta;
+        fcast.plc = origin.plc;
+/*
+    forecast timeslots in UTC for a single day are - 
+    00:00 
+    03:00
+    06:00
+    09:00
+    12:00
+    15:00
+    18:00
+    21:00
+    
+    first forecast in array should be the next timeslot, for
+    example : if the current time is 14:21 the first timeslot
+    in the list array will/should be 15:00
+    
+    there can be up to 40 timeslots : 
+         5 days(24hr periods) X 8 slots/day(3hr slots)
+    
+    the number of timeslots can vary, it depends on what the
+    current timeslot is when the data was requested
+    
+    the virtual index(currently not used) will be a value from
+    0 to 7. it indicates where to place the real-index of 0. use 
+    it as an offset to the real-index.
+    virtual timeslot index = round((epoch_now - epoch_last_midnght) / (3 * 3600))
+    
+    stopix = 3(24hr periods) X 8(timeslots)
+    for(fcix = 0; fcix < stopix; fcix++)
+    
+    NOTE : openwm time in forecast is UTC!
+    let d = new Date(0);
+    d.setUTCSeconds(data.list[fcix].dt);
+    fcast.gmt = d.getTime();
+
+        fcast = {
+            sta: ,
+            plc: ,
+            svc: ,
+            cnt: ,
+            per: [],
+            tstamp: 
+        }
+
+        per = {
+            slot: , // currently unused
+            dt: ,
+            icon: ,
+            t: ,
+            h: ,
+            tmin: ,
+            tmax: ,
+            ws: ,
+            wd: ,
+            main: ,
+            desc: 
+        }
+*/
+        // date/time of when data was collected
+        fcast.gmt = fcast.tstamp = Date.now();
+
+        // qty of time slots  cnt = 3(24hr periods) X 8(timeslots)
+        fcast.cnt = 24;
+
+        fcast.per = [];
+
+        for(let ix = 0;ix < fcast.cnt; ix++) {
+            // date/time of forecast
+            // NOTE : openwm time in forecast is UTC!
+            let d = new Date(0);
+            d.setUTCSeconds(raw.list[ix].dt);
+            per.dt   = d.getTime();
+            per.icon = wcfg.service.iconurl + raw.list[ix].weather[0].icon +'.png';
+            per.t    = raw.list[ix].main.temp;
+            per.h    = raw.list[ix].main.humidity;
+            per.tmin = raw.list[ix].main.temp_min;
+            per.tmax = raw.list[ix].main.temp_max;
+            per.ws   = raw.list[ix].wind.speed;
+            per.wd   = raw.list[ix].wind.deg;
+            // weather[] can have more than just one 
+            // entry. not sure yet how to combine them 
+            // into usable data because the docs don't 
+            // have enough info and the occurrence is rare.
+            per.main = raw.list[ix].weather[0].main;
+            per.desc = raw.list[ix].weather[0].description;
+
+            fcast.per.push(JSON.parse(JSON.stringify(per)));
+            per = {};
+        }
+        // break the reference and notify...
+        wxsvc.forecast = JSON.parse(JSON.stringify(fcast));
+        sys_evts.emit('WSVC_FORCST', wxsvc.forecast);
+    };
+
+    return wxsvc;
+})();
